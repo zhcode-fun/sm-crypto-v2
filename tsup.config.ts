7009eeb69553240d2dcf6730feaa2ca217dc183e
haotianlong <haotianlong@tencent.com>
2023-06-04 14:25:42 +0800

    chore: remove from dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,12 +4,11 @@ export default defineConfig({
   entry: ['./src/index.ts'],
   clean: true,
   outDir: 'dist',
-  noExternal: ['jsbn'],
   dts: true,
   // we need to keep minify false, since webpack magic comments
   // will be stripped if minify.
   minify: false,
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   target: 'esnext',
   tsconfig: 'tsconfig.json',
   esbuildOptions(options) {
