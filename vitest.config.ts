73b83fa2b30db8311365841c603589f525c2d116
haotianlong <haotianlong@tencent.com>
2023-06-06 14:54:03 +0800

    fix(test): summary

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
         reporter: [
           ['text'],
           ['html'],
-          ['json', { 'file': 'coverage-summary.json' }],
+          ['json-summary'],
         ]
       }
     },
